051643aedef0b04d9d5b0ae4b9ac842689004bc1
Florin Buga <7334553+finnbuga@users.noreply.github.com>
2015-02-19 22:43:45 +0200

    Initial commit.

---
+++ config.rb
@@ -0,0 +1,15 @@
+# Compass CSS framework config file
+
+require "susy"
+require "bourbon"
+
+# Require any additional compass plugins here.
+
+project_type = :stand_alone
+http_path = "/"
+sass_dir = "."
+css_dir = "."
+preferred_syntax = :scss
+output_style = :expanded
+relative_assets = true
+line_comments = false
