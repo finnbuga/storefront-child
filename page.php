78f3c0ad6e0ea3feb48c7701b3eb75270ceda697
Florin Buga <7334553+finnbuga@users.noreply.github.com>
2015-03-03 17:33:51 +0200

    Set the default page to be full width (no Widget Area).

---
+++ page.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * The template for displaying full width pages.
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+			<?php while ( have_posts() ) : the_post(); ?>
+
+				<?php
+				do_action( 'storefront_page_before' );
+				?>
+
+				<?php get_template_part( 'content', 'page' ); ?>
+
+				<?php
+				/**
+				 * @hooked storefront_display_comments - 10
+				 */
+				do_action( 'storefront_page_after' );
+				?>
+
+			<?php endwhile; // end of the loop. ?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
