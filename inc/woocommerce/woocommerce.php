60343a17b8d495076af2b338c2b0867afd408572
Florin Buga <7334553+finnbuga@users.noreply.github.com>
2015-03-10 21:57:10 +0200

    Hide unnecessary fields from the checkout form.

--- inc/woocommerce/woocommerce.php
+++ inc/woocommerce/woocommerce.php
@@ -7,3 +7,14 @@ function storefront_child_woocommerce_load_styles() {
 	wp_enqueue_style(  'storefront-child-woocommerce', get_stylesheet_directory_uri() . '/inc/woocommerce/woocommerce.css' );
 }
 add_action( 'wp_enqueue_scripts', 'storefront_child_woocommerce_load_styles', 30 );
+
+
+/**
+ * Make the postcode not mandatory on the checkout form.
+ */
+function billing_postcode( $address_fields ) {
+	$address_fields['postcode']['required'] = 0;
+//	print_r($address_fields);
+	return $address_fields;
+}
+add_filter( 'woocommerce_default_address_fields', 'billing_postcode' );
