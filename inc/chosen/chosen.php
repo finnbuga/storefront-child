6bffc2814248caa55ef23bbe14f26a03e58b58a4
Florin Buga <7334553+finnbuga@users.noreply.github.com>
2015-03-11 18:03:47 +0200

    Move Chosen related CSS into inc/chosen.

--- inc/chosen/chosen.php
+++ inc/chosen/chosen.php
@@ -4,8 +4,12 @@
  * Load stylesheets and scripts.
  */
 function storefront_child_chosen_load_styles_and_scripts() {
+	// Load vendor styles and script.
 	wp_enqueue_style(  'chosen',                  get_stylesheet_directory_uri() . '/inc/chosen/vendor/chosen/chosen.css' );
 	wp_enqueue_script( 'chosen',                  get_stylesheet_directory_uri() . '/inc/chosen/vendor/chosen/chosen.jquery.js', array( 'jquery' ) );
+
+	// Load styles and script specific to this theme.
+	wp_enqueue_style(  'storefront-child-chosen', get_stylesheet_directory_uri() . '/inc/chosen/chosen.css', array( 'chosen' ) );
 	wp_enqueue_script( 'storefront-child-chosen', get_stylesheet_directory_uri() . '/inc/chosen/chosen.js', array( 'chosen' ) );
 }
 add_action( 'wp_enqueue_scripts', 'storefront_child_chosen_load_styles_and_scripts' );
