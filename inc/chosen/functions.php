623a14e78caccc8d21593773578e87e6df2ab5b5
Florin Buga <7334553+finnbuga@users.noreply.github.com>
2015-03-15 22:33:59 +0200

    Move scripts from the <head>

--- inc/chosen/functions.php
+++ inc/chosen/functions.php
@@ -8,10 +8,10 @@
  */
 function storefront_child_chosen_load_styles_and_scripts() {
 	// Load vendor styles and script.
-	wp_enqueue_style(  'chosen',                  get_stylesheet_directory_uri() . '/inc/chosen/vendor/chosen/chosen.css' );
-	wp_enqueue_script( 'chosen',                  get_stylesheet_directory_uri() . '/inc/chosen/vendor/chosen/chosen.jquery.js', array( 'jquery' ) );
+	wp_enqueue_style(  'chosen', get_stylesheet_directory_uri() . '/inc/chosen/vendor/chosen/chosen.css' );
+	wp_enqueue_script( 'chosen', get_stylesheet_directory_uri() . '/inc/chosen/vendor/chosen/chosen.jquery.js', array( 'jquery' ), '20150315', true );
 
 	// Load styles and script specific to this theme.
 	wp_enqueue_style(  'storefront-child-chosen', get_stylesheet_directory_uri() . '/inc/chosen/chosen.css', array( 'chosen' ) );
-	wp_enqueue_script( 'storefront-child-chosen', get_stylesheet_directory_uri() . '/inc/chosen/chosen.js', array( 'chosen' ) );
+	wp_enqueue_script( 'storefront-child-chosen', get_stylesheet_directory_uri() . '/inc/chosen/chosen.js', array( 'chosen' ), '20150315', true );
 }
