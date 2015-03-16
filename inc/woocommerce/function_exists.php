0bffc6fef437362c6d8f9082b8b503b299309b7d
Florin Buga <7334553+finnbuga@users.noreply.github.com>
2015-03-16 23:53:22 +0200

    Display 4 related products instead of 2.

--- inc/woocommerce/function_exists.php
+++ inc/woocommerce/function_exists.php
@@ -9,3 +9,13 @@
 function woocommerce_template_loop_product_thumbnail() {
 	echo '<figure>' . woocommerce_get_product_thumbnail() . '</figure>';
 }
+
+/**
+ * Redefine the number of related products displayed.
+ */
+function woocommerce_output_related_products() {
+	woocommerce_related_products( array(
+		'posts_per_page' => 4,
+		'columns'        => 4
+	) );
+}
